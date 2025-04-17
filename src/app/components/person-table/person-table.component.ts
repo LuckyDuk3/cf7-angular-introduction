6ba4d60c4737f170ab4a1a2be658ac41412f4956
LuckyDuk3 <dio.doukas@gmail.com>
2025-04-17 12:03:48 +0300

    New component person table component

---
+++ src/app/components/person-table/person-table.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-table',
+  imports: [],
+  templateUrl: './person-table.component.html',
+  styleUrl: './person-table.component.css'
+})
+export class PersonTableComponent {
+  name = "Dio"
+
+  person = {
+    givenName: "Dio",
+    surName: "Black",
+    age: 99,
+    email: "dio@mail.com"
+  }
+}
